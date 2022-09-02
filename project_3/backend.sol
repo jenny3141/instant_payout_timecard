ae378c28888e662cfb22c8b988e97b087a3e0a67
0xSlashSlash <101845770+0xSlashSlash@users.noreply.github.com>
2022-09-02 18:45:29 -0400

    Add files via upload

---
+++ project_3/backend.sol
@@ -0,0 +1,195 @@
+pragma solidity 0.8.4;
+import "https://github.com/nibbstack/erc721/blob/master/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/nibbstack/erc721/blob/master/src/contracts/ownership/ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+//Base nft which reflects employment, would be admin minted to specific employees upon hiring and then burned upon firing.
+contract newNFT is NFTokenMetadata, Ownable{
+    constructor() {
+        nftName = "Timecard";
+        nftSymbol = "TC";
+    }
+    //strcture utilized to be able to map wages to specific employee addresses; needs editing.
+    //struct _wage{
+        //address addr;
+        //uint256 _wage;
+    }
+    function mint(address _to, uint256 _tokenId, string calldata _uri, uint256 _wage4) external onlyOwner {
+        super._mint(_to, _tokenId);
+        super._setTokenUri(_tokenId, _uri);
+    }
+    function burn(uint256 _tokenId) external onlyOwner {
+        super._burn(_tokenId);
+    }
+    //mapping was not correctly set to function will not work properly
+    function setWage(uint256 _tokenId, uint _newwage, address _to) public {
+        _wage[_to] = _newwage;
+    }
+}
+//token meant to represent employee rewards which they would then be able to pull from a liquidity pool.
+contract PseudoDollar is ERC20 {
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+    {
+        _mint(msg.sender, 100000000 * 18**decimals());
+    }
+}
+//generic staking contract that is timestamp based so as to function off of wage*hours worked. The idea here is that you would stake upon beginning your work cycle and then unstaking once done for the day.
+contract ERC721Staking is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    // Interfaces for ERC20 and ERC721
+    IERC20 public immutable rewardsToken;
+    IERC721 public immutable nftCollection;
+
+    // Staker info
+    struct Staker {
+        // Amount of ERC721 Tokens staked
+        uint256 amountStaked;
+        // Last time of details update for this User
+        uint256 timeOfLastUpdate;
+        // Calculated, but unclaimed rewards for the User. The rewards are
+        // calculated each time the user writes to the Smart Contract
+        uint256 unclaimedRewards;
+    }
+
+    // Rewards per hour per token deposited in wei.
+    // Rewards are cumulated once every hour.
+    uint256 private rewardsPerHour = 100000;
+
+    // Mapping of User Address to Staker info
+    mapping(address => Staker) public stakers;
+    // Mapping of Token Id to staker. Made for the SC to remeber
+    // who to send back the ERC721 Token to.
+    mapping(uint256 => address) public stakerAddress;
+
+    address[] public stakersArray;
+
+    // Constructor function
+    constructor(IERC721 _nftCollection, IERC20 _rewardsToken) {
+        nftCollection = _nftCollection;
+        rewardsToken = _rewardsToken;
+    }
+
+    // If address already has ERC721 Token/s staked, calculate the rewards.
+    // For every new Token Id in param transferFrom user to this Smart Contract,
+    // increment the amountStaked and map msg.sender to the Token Id of the staked
+    // Token to later send back on withdrawal. Finally give timeOfLastUpdate the
+    // value of now.
+    function stake(uint256[] calldata _tokenIds) external nonReentrant {
+        if (stakers[msg.sender].amountStaked > 0) {
+            uint256 rewards = calculateRewards(msg.sender);
+            stakers[msg.sender].unclaimedRewards += rewards;
+        } else {
+            stakersArray.push(msg.sender);
+        }
+        uint256 len = _tokenIds.length;
+        for (uint256 i; i < len; ++i) {
+            require(
+                nftCollection.ownerOf(_tokenIds[i]) == msg.sender,
+                "Can't stake tokens you don't own!"
+            );
+            nftCollection.transferFrom(msg.sender, address(this), _tokenIds[i]);
+            stakerAddress[_tokenIds[i]] = msg.sender;
+        }
+        stakers[msg.sender].amountStaked += len;
+        stakers[msg.sender].timeOfLastUpdate = block.timestamp;
+    }
+
+    // Check if user has any ERC721 Tokens Staked and if he tried to withdraw,
+    // calculate the rewards and store them in the unclaimedRewards and for each
+    // ERC721 Token in param: check if msg.sender is the original staker, decrement
+    // the amountStaked of the user and transfer the ERC721 token back to them
+    function withdraw(uint256[] calldata _tokenIds) external nonReentrant {
+        require(
+            stakers[msg.sender].amountStaked > 0,
+            "You have no tokens staked"
+        );
+        uint256 rewards = calculateRewards(msg.sender);
+        stakers[msg.sender].unclaimedRewards += rewards;
+        uint256 len = _tokenIds.length;
+        for (uint256 i; i < len; ++i) {
+            require(stakerAddress[_tokenIds[i]] == msg.sender);
+            stakerAddress[_tokenIds[i]] = address(0);
+            nftCollection.transferFrom(address(this), msg.sender, _tokenIds[i]);
+        }
+        stakers[msg.sender].amountStaked -= len;
+        stakers[msg.sender].timeOfLastUpdate = block.timestamp;
+        if (stakers[msg.sender].amountStaked == 0) {
+            for (uint256 i; i < stakersArray.length; ++i) {
+                if (stakersArray[i] == msg.sender) {
+                    stakersArray[i] = stakersArray[stakersArray.length - 1];
+                    stakersArray.pop();
+                }
+            }
+        }
+    }
+
+    // Calculate rewards for the msg.sender, check if there are any rewards
+    // claim, set unclaimedRewards to 0 and transfer the ERC20 Reward token
+    // to the user.
+    function claimRewards() external {
+        uint256 rewards = calculateRewards(msg.sender) +
+            stakers[msg.sender].unclaimedRewards;
+        require(rewards > 0, "You have no rewards to claim");
+        stakers[msg.sender].timeOfLastUpdate = block.timestamp;
+        stakers[msg.sender].unclaimedRewards = 0;
+        rewardsToken.safeTransfer(msg.sender, rewards);
+    }
+
+    // Set the rewardsPerHour variable
+    // Because the rewards are calculated passively, the owner has to first update the rewards
+    // to all the stakers, witch could result in very heavy load and expensive transactions
+    function setRewardsPerHour(uint256 _newValue) public onlyOwner {
+        address[] memory _stakers = stakersArray;
+        uint256 len = _stakers.length;
+        for (uint256 i; i < len; ++i) {
+            address user = _stakers[i];
+            stakers[user].unclaimedRewards += calculateRewards(user);
+            stakers[msg.sender].timeOfLastUpdate = block.timestamp;
+        }
+        rewardsPerHour = _newValue;
+    }
+
+    //////////
+    // View //
+    //////////
+
+    function userStakeInfo(address _user)
+        public
+        view
+        returns (uint256 _tokensStaked, uint256 _availableRewards)
+    {
+        return (stakers[_user].amountStaked, availableRewards(_user));
+    }
+
+    function availableRewards(address _user) internal view returns (uint256) {
+        if (stakers[_user].amountStaked == 0) {
+            return stakers[_user].unclaimedRewards;
+        }
+        uint256 _rewards = stakers[_user].unclaimedRewards +
+            calculateRewards(_user);
+        return _rewards;
+    }
+
+    /////////////
+    // Internal//
+    /////////////
+
+    // Calculate rewards for param _staker by calculating the time passed
+    // since last update in hours and mulitplying it to ERC721 Tokens Staked
+    // and rewardsPerHour.
+    function calculateRewards(address _staker)
+        internal
+        view
+        returns (uint256 _rewards)
+    {
+        Staker memory staker = stakers[_staker];
+        return (((
+            ((block.timestamp - staker.timeOfLastUpdate) * staker.amountStaked)
+        ) *  _wage[staker]) / 3600);  //will not function properly as mapping needs editing as mentioned above.
+    }
+}
